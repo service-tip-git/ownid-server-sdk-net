f0ca53004aa3e14a693a0848402e2f7e001f5de5
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-07-22 15:59:40 +0300

    deployment fix

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -30,7 +30,7 @@ kubectl -n=$ENV set image deployment/ownid-server-netcore3-gigya-deployment owni
 kubectl -n=$ENV set image deployment/ownid-server-netcore3-gigya-2-deployment ownid-server-netcore3-gigya-2=$REPOSITORY_URI:$IMAGE_TAG --record
 kubectl -n=$ENV set image deployment/ownid-server-netcore3-demo-gigya-deployment ownid-server-netcore3-demo-gigya=$REPOSITORY_URI:$IMAGE_TAG --record
 
-if [ "$ENV" == "staging" ]; then
+if [ "$ENV" = "staging" ]; then
         kubectl -n=$ENV set image deployment/ownid-server-netcore3-nestle-deployment ownid-server-netcore3-nestle=$REPOSITORY_URI:$IMAGE_TAG --record
 fi
 
