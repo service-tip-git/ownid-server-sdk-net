a685803e2f788eb41b286a8c0265ae0e20c74e55
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-09-24 17:15:46 +0300

    publish cleanup

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,14 +4,14 @@ REPOSITORY_URI=$1
 IMAGE_TAG=$2
 ENV=$3
 
-PKG_VERSION=`xmllint --xpath "string(//Project/PropertyGroup/Version)" ./OwnIdSdk.NetCore3.Web/OwnIdSdk.NetCore3.Web.csproj`
-S3PATH=s3://ownid-sdks-bucket/$ENV/server-sdks/dotnetcore3
-FOLDER=$PKG_VERSION"_"$TRAVIS_BUILD_NUMBER
+# PKG_VERSION=`xmllint --xpath "string(//Project/PropertyGroup/Version)" ./OwnIdSdk.NetCore3.Web/OwnIdSdk.NetCore3.Web.csproj`
+# S3PATH=s3://ownid-sdks-bucket/$ENV/server-sdks/dotnetcore3
+# FOLDER=$PKG_VERSION"_"$TRAVIS_BUILD_NUMBER
 
-echo Path: $S3PATH/$FOLDER
+#echo Path: $S3PATH/$FOLDER
 
 # Publish Netcore3 library
-dotnet build  --configuration Release --version-suffix ci-build
+#dotnet build  --configuration Release --version-suffix ci-build
 
 # aws s3 cp ./OwnIdSdk.NetCore3.Web/bin/Release/netcoreapp3.1 $S3PATH/latest --recursive
 # aws s3 cp ./OwnIdSdk.NetCore3.Web/bin/Release/netcoreapp3.1 $S3PATH/$FOLDER --recursive
@@ -23,8 +23,6 @@ docker push $REPOSITORY_URI:$IMAGE_TAG
 
 echo Updating image in Cluster deployment
 kubectl apply -f manifests/$ENV.yaml
-
-sleep 3
 
 kubectl -n=$ENV set image deployment/ownid-server-netcore3-gigya-deployment ownid-server-netcore3-gigya=$REPOSITORY_URI:$IMAGE_TAG --record
 # kubectl -n=$ENV set image deployment/ownid-server-netcore3-gigya-2-deployment ownid-server-netcore3-gigya-2=$REPOSITORY_URI:$IMAGE_TAG --record
