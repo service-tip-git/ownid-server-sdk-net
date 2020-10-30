9d759ead1a5e7b974dc31ff782e75a27c18aae35
Vitalii Marynkevych <vitalii.marynkevych@sap.com>
2020-10-30 17:17:57 +0200

    updated publish.sh (#300)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -31,6 +31,9 @@ kubectl -n=$ENV set image deployment/ownid-server-netcore3-gigya-deployment owni
 # kubectl -n=$ENV set image deployment/ownid-server-netcore3-gigya-2-deployment ownid-server-netcore3-gigya-2=$REPOSITORY_URI:$IMAGE_TAG --record
 kubectl -n=$ENV set image deployment/ownid-server-netcore3-demo-gigya-deployment ownid-server-netcore3-demo-gigya=$REPOSITORY_URI:$IMAGE_TAG --record
 kubectl -n=$ENV set image deployment/ownid-server-netcore3-demo-2-gigya-deployment ownid-server-netcore3-demo-2-gigya=$REPOSITORY_URI:$IMAGE_TAG --record
+kubectl -n=$ENV set image deployment/ownid-server-netcore3-demo-3-gigya-deployment ownid-server-netcore3-demo-3-gigya=$REPOSITORY_URI:$IMAGE_TAG --record
+kubectl -n=$ENV set image deployment/ownid-server-netcore3-demo-4-gigya-deployment ownid-server-netcore3-demo-4-gigya=$REPOSITORY_URI:$IMAGE_TAG --record
+
 
 if [ "$ENV" = "staging" ]; then
         kubectl -n=$ENV set image deployment/gigyapoc-deployment gigyapoc=$REPOSITORY_URI:$IMAGE_TAG --record
