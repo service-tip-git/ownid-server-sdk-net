6bcc897337182e0222448cfa3dd9a3729ed09bb4
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-11-10 15:43:27 +0200

    deploy.sh cleanup

--- scripts/prod/deploy.sh
+++ scripts/prod/deploy.sh
@@ -1,12 +1,5 @@
 #!bin/sh
 
-echo Adding K8S clusters
-echo Prod A
-aws eks --region us-east-1 update-kubeconfig --name ownid-production-cluster
-echo Prod B
-aws eks --region us-east-2 update-kubeconfig --name ownid-eks
-
-#Deploy Netcore3 Server-Gigya
 PKG_VERSION=`xmllint --xpath "string(//Project/PropertyGroup/AssemblyVersion)" ./OwnIdSdk.NetCore3.Server.Gigya/OwnIdSdk.NetCore3.Server.Gigya.csproj`
 IMAGE_URI=$ARTIFACTORY_URL/prod/server/ownid-server-gigya_${PKG_VERSION-}:$TRAVIS_COMMIT
 
@@ -21,8 +14,8 @@ kubectl config get-contexts
 
 echo Prod A Deployment
 kubectl config use-context arn:aws:eks:us-east-1:571861302935:cluster/ownid-production-cluster
-sh scripts/prod/k8s-update.sh $IMAGE_URI
+sh scripts/prod/cluster-update.sh $IMAGE_URI
 
 echo Prod B Deployment
 kubectl config use-context arn:aws:eks:us-east-2:571861302935:cluster/ownid-eks
-sh scripts/prod/k8s-update.sh $IMAGE_URI
+sh scripts/prod/cluster-update.sh $IMAGE_URI
