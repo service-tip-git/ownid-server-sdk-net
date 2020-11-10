232030f6a2481f6a301a47e8478a23c4f8703384
Bohdan Donchenko <bohdan.donchenko@sap.com>
2020-11-10 14:11:41 +0200

    added release deployment changes

---
+++ scripts/prod/deploy.sh
@@ -0,0 +1,36 @@
+#!bin/sh
+
+echo Adding K8S clusters
+echo Prod A
+aws eks --region us-east-1 update-kubeconfig --name ownid-production-cluster
+echo Prod B
+aws eks --region us-east-2 update-kubeconfig --name ownid-eks
+
+#Deploy Netcore3 Server-Gigya
+PKG_VERSION=`xmllint --xpath "string(//Project/PropertyGroup/AssemblyVersion)" ./OwnIdSdk.NetCore3.Server.Gigya/OwnIdSdk.NetCore3.Server.Gigya.csproj`
+IMAGE_URI=$ARTIFACTORY_URL/prod/server/ownid-server-gigya_${PKG_VERSION-}:$TRAVIS_COMMIT
+
+echo Docker push to $IMAGE_URI
+docker tag ownid-server-gigya:latest $IMAGE_URI
+docker push $IMAGE_URI
+
+echo Adding K8S clusters...
+aws eks --region us-east-1 update-kubeconfig --name ownid-production-cluster
+aws eks --region us-east-2 update-kubeconfig --name ownid-eks
+kubectl config get-contexts
+
+echo Prod A Deployment
+echo K8S cluster selection
+kubectl config use-context arn:aws:eks:us-east-1:571861302935:cluster/ownid-production-cluster
+
+pwd
+
+sh scripts/prod/k8s-update.sh $IMAGE_URI
+
+echo Prod B Deployment
+echo K8S cluster selection
+kubectl config use-context arn:aws:eks:us-east-2:571861302935:cluster/ownid-eks
+
+pwd
+
+sh scripts/prod/k8s-update.sh $IMAGE_URI
