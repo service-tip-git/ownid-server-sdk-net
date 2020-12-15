330c8bc501be454da75488738f64f0ce66fd8383
Vitalii Marynkevych <vitalii.marynkevych@sap.com>
2020-12-15 13:10:10 +0200

    added reflection call for FlowController to reinit on ConfigInjectionMiddleware call (#422)

--- OwnID.Server.Gigya/Middlewares/ConfigInjectionMiddleware.cs
+++ OwnID.Server.Gigya/Middlewares/ConfigInjectionMiddleware.cs
@@ -1,17 +1,22 @@
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using OwnID.Extensibility.Configuration;
 using OwnID.Extensibility.Json;
+using OwnID.Flow.Interfaces;
 
 namespace OwnID.Server.Gigya.Middlewares
 {
     public class ConfigInjectionMiddleware
     {
         private readonly IOwnIdCoreConfiguration _configuration;
+        private readonly IFlowController _flowController;
 
-        public ConfigInjectionMiddleware(RequestDelegate next, IOwnIdCoreConfiguration configuration)
+        public ConfigInjectionMiddleware(RequestDelegate next, IOwnIdCoreConfiguration configuration,
+            IFlowController flowController)
         {
             _configuration = configuration;
+            _flowController = flowController;
         }
 
         public async Task Invoke(HttpContext context)
@@ -21,13 +26,17 @@ namespace OwnID.Server.Gigya.Middlewares
             _configuration.TFAEnabled = config.TFAEnabled;
             _configuration.Fido2FallbackBehavior = config.Fido2FallbackBehavior;
 
+            var initFlowControllerMethod = _flowController.GetType()
+                .GetMethod("InitMap", BindingFlags.NonPublic | BindingFlags.Instance);
+            initFlowControllerMethod!.Invoke(_flowController, null);
+
             context.Response.StatusCode = StatusCodes.Status204NoContent;
         }
-        
+
         private class ConfigToInject
         {
             public bool TFAEnabled { get; set; }
-            
+
             public Fido2FallbackBehavior Fido2FallbackBehavior { get; set; }
         }
     }
