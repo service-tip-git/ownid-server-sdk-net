5d8c63ae9788896b62ce47ba537008cf2a6d9668
Andrii Vospitannikov <andrii.vospitannikov@sap.com>
2021-01-13 18:46:05 +0200

    ownid-webapp/#111: Replace 'You're logged in' message when not logged in - bugfix (#82)
    bugfix

--- OwnID/Commands/SavePartialConnectionCommand.cs
+++ OwnID/Commands/SavePartialConnectionCommand.cs
@@ -23,25 +23,23 @@ namespace OwnID.Commands
         public async Task ExecuteAsync(UserIdentitiesData input, CacheItem relatedItem)
         {
             var recoveryToken = !string.IsNullOrEmpty(input.RecoveryData) ? Guid.NewGuid().ToString("N") : null;
-
-            var challengeType = relatedItem.ChallengeType;
             
             // Credentials created at web app
             if (input.ActualChallengeType.HasValue && input.ActualChallengeType != relatedItem.ChallengeType)
             {
-                challengeType = input.ActualChallengeType.Value;
+                relatedItem.ChallengeType = input.ActualChallengeType.Value;
             }
             // check if users exists or not. If not - then this is LinkOnLogin
             else if (relatedItem.ChallengeType == ChallengeType.Login)
             {
                 var isUserExists = await _userHandlerAdapter.IsUserExistsAsync(input.PublicKey);
                 if (!isUserExists)
-                    challengeType = ChallengeType.LinkOnLogin;
+                    relatedItem.ChallengeType = ChallengeType.LinkOnLogin;
             }
-
+            
             await _cacheItemRepository.UpdateAsync(relatedItem.Context, item =>
             {
-                item.ChallengeType = challengeType;
+                item.ChallengeType = relatedItem.ChallengeType;
                 item.RecoveryToken = recoveryToken;
                 item.RecoveryData = input.RecoveryData;
                 item.FinishFlow(input.DID, input.PublicKey);
